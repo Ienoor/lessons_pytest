bb4cb0e2c4331463695c24f862adfa83e9a49bb3
sol <tyaptyapych@gmail.com>
2023-09-06 18:09:09 +0300

    created a test to determine the number of posts

--- src/enums/errors.py
+++ src/enums/errors.py
@@ -3,3 +3,4 @@ from enum import Enum
 
 class GlobalError(Enum):
     WRONG_STATUS_CODE = "Status code is not equal expected."
+    WRONG_NUMBER_OF_POSTS = "The number of posts does not equal the expected number of posts."
--- tests/test_something.py
+++ tests/test_something.py
@@ -4,7 +4,13 @@ import requests
 from config import SERVICE_URL
 from src.enums.errors import GlobalError
 
+response = requests.get(SERVICE_URL)
 
-def test_getting_posts():
-    response = requests.get(SERVICE_URL)
+
+def test_getting_status_code():
     assert response.status_code == 200, GlobalError.WRONG_STATUS_CODE.value
+
+
+def test_getting_count_posts():
+    received_posts = response.json()
+    assert len(received_posts) == 4, GlobalError.WRONG_NUMBER_OF_POSTS.value
