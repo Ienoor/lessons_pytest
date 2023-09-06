397d8044d8fbc73e1a650c8e8e00f5ff5c0fd08e
sol <tyaptyapych@gmail.com>
2023-09-06 19:02:54 +0300

    code update

--- tests/base/response.py
+++ tests/base/response.py
@@ -1,14 +1,23 @@
 from jsonschema import validate
 
+from tests.enums.errors import GlobalError
+
 
 class Response:
-    def __init__(self, response, schema):
-        self.response = response
-        self.schema = schema
+    def __init__(self, response):
+        self.response = response.json()
+        self.status_code = response.status_code
 
-    def validate(self):
+    def validate(self, schema):
         if isinstance(self.response, list):
             for item in self.response:
-                validate(item, self.schema)
+                validate(item, schema)
         else:
-            validate(self.response, self.schema)
+            validate(self.response, schema)
+
+    def assert_status_code(self, status_code):
+        if isinstance(status_code, list):
+            assert self.status_code in status_code, GlobalError.WRONG_STATUS_CODE.value
+
+    def assert_count_items(self, count):
+        assert len(self.response) == 3, GlobalError.WRONG_NUMBER_OF_POSTS.value
--- tests/test_something.py
+++ tests/test_something.py
@@ -3,21 +3,18 @@ import requests
 
 from config import SERVICE_URL
 from tests.base.response import Response
-from tests.enums.errors import GlobalError
 from tests.schemas.Posts import POST_SCHEMA
 
 response = requests.get(SERVICE_URL)
 
 
 def test_getting_status_code():
-    assert response.status_code == 200, GlobalError.WRONG_STATUS_CODE.value
+    Response(response).assert_status_code(200)
 
 
 def test_getting_count_posts():
-    received_posts = response.json()
-    assert len(received_posts) == 3, GlobalError.WRONG_NUMBER_OF_POSTS.value
+    Response(response).assert_count_items(3)
 
 
 def test_validate_data():
-    res = Response(response.json(), POST_SCHEMA)
-    res.validate()
+    Response(response).validate(POST_SCHEMA)
